d9bf5506bedfc6e33369ed1dd6366519f9b476c0
Robert White <robert@terracoding.com>
2015-12-04 12:21:07 +0000

    Configure Rspec

---
+++ spec/dummy/spec/truthy_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe true do
+  it 'is truthy' do
+    expect(true).to be_truthy
+  end
+end
---
+++ spec/truthy_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe NaturalResource do
+  it 'is a Module' do
+    expect(NaturalResource).to be_a Module
+  end
+end
