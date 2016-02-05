76f0878098b0250f8d7ebfc2c413394ac135bdef
Robert White <robert@terracoding.com>
2016-02-05 14:43:25 +0000

    Add missing Rspec pundit helper

--- lib/generators/natural/install/install_generator.rb
+++ lib/generators/natural/install/install_generator.rb
@@ -5,5 +5,6 @@ class Natural::InstallGenerator < Rails::Generators::Base
 
   def copy_application_policy
     template 'application_policy.rb', 'app/policies/application_policy.rb'
+    template 'pundit.rb', 'spec/support/pundit.rb'
   end
 end
