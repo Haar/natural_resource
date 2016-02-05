f42c57588213e9b9d67364e1115278f3ca75604c
Robert White <robert@terracoding.com>
2016-02-05 15:04:51 +0000

    0.1.6 Release: Fix install generator
    * Load support files if loader isn’t included

--- lib/generators/natural/install/install_generator.rb
+++ lib/generators/natural/install/install_generator.rb
@@ -5,6 +5,16 @@ class Natural::InstallGenerator < Rails::Generators::Base
 
   def copy_application_policy
     template 'application_policy.rb', 'app/policies/application_policy.rb'
-    template 'pundit.rb', 'spec/support/pundit.rb'
+    helper_path = Rails.root.join('spec', 'rails_helper.rb')
+    if File.exists?(helper_path)
+      template('pundit.rb', 'spec/support/pundit.rb')
+      helper_content = File.read(helper_path)
+      support_file_loader = "Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }"
+      unless helper_content.include?(support_file_loader)
+        helper_content.gsub!("require 'rspec/rails'",
+                             "require 'rspec/rails'\n#{support_file_loader}")
+        File.open(helper_path, 'w') { |f| f.puts helper_content }
+      end
+    end
   end
 end
